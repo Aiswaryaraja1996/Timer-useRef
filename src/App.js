c75a3a64473955393b4e0b7cea90a7e1a76886cb
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2021-11-28 23:02:11 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [state, setState] = useState(0);
+  const timeRef = useRef(null);
+
+  useEffect(() => {
+    return pauseTimer;
+  }, []);
+  const startTimer = () => {
+    if (timeRef.current === null) {
+      timeRef.current = setInterval(() => {
+        setState((prev) => prev + 1);
+      }, 1000);
+    }
+  };
+  const pauseTimer = () => {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  };
+
+  const resetTimer = () => {
+    pauseTimer();
+    setState(0);
+  };
+  const ref = useRef(null);
+
+  const handleMovetoTop = () => {
+    if (ref.current) {
+      ref.current.scrollTop = 0;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>{state}</div>
+      <button onClick={startTimer}>START</button>
+      <button onClick={pauseTimer}>PAUSE</button>
+      <button onClick={resetTimer}>RESET</button>
+      <br/>
+      <br/>
+      <div>
+        <button onClick={handleMovetoTop}>Move to Top</button>
+      <br/>
+      <br/>
+        <div
+          ref={ref}
+          style={{
+            maxHeight: 200,
+            overflow: "auto",
+            border: "1px solid black",
+            padding: 20,
+            width: 200,
+            margin:"auto"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{ height: "auto", border: "1px solid black" }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. Contrary to popular
+            belief, Lorem Ipsum is not simply random text. It has roots in a
+            piece of classical Latin literature from 45 BC, making it over 2000
+            years old. Richard McClintock, a Latin professor at Hampden-Sydney
+            College in Virginia, looked up one of the more obscure Latin words,
+            consectetur, from a Lorem Ipsum passage, and going through the cites
+            of the word in classical literature, discovered the undoubtable
+            source.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
